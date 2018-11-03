92e2e53bc89e900b6d485807cf026acf984b243d
Геннадий Максимов <mr.gennadiy.maksimov@gmail.com>
2018-11-04 02:00:45 +0300

    векторная графика и оптимизация

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,25 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+
+gulp.task("webp", function(){
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 70}))
+    .pipe(gulp.dest("source/img"))
+});
+
+gulp.task("images", function(){
+  return gulp.src("source/img/**/*.{png, jpg, svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+      ]))
+
+    .pipe(gulp.dest("source/img"));
+});
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
