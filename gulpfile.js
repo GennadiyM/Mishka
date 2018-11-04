277566977ea0510ba3389dfcf750c62e4008a6df
Геннадий Максимов <mr.gennadiy.maksimov@gmail.com>
2018-11-04 09:18:22 +0300

    Автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,36 @@ let autoprefixer = require("autoprefixer");
 let server = require("browser-sync").create();
 let csso = require("gulp-csso");
 let rename = require("gulp-rename");
+let imagemin = require("gulp-imagemin");
+let webp = require("gulp-webp");
+let svgstore = require("gulp-svgstore");
+let posthtml = require("gulp-posthtml");
+let include = require("posthtml-include");
+
+gulp.task("sprite", function(){
+  return gulp.src("source/img/sprite-*")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function(){
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 70}))
+    .pipe(gulp.dest("source/img"))
+});
+
+gulp.task("images", function(){
+  return gulp.src("source/img/**/*.{png, jpg, svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+      ]))
+    .pipe(gulp.dest("source/img"));
+});
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -21,6 +51,14 @@ gulp.task("css", function () {
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
+});
+
+gulp.task("html", function(){
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+  ]))
+    .pipe(gulp.dest("source"));
 });
 
 gulp.task("server", function () {
